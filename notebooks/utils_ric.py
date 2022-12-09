1a1799529bfc9575f0eeec6d4686659b2e8cea47
ricardino <ricardo.montoya.da@gmail.com>
2022-12-09 13:11:36 +0100

    added patient object and made a simple registration pipeline

--- notebooks/utils_ric.py
+++ notebooks/utils_ric.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import SimpleITK as sitk
+import pickle
+import numpy as np
 
 notebooks_path = Path.cwd()
 repo_path = notebooks_path.parent
@@ -14,3 +16,40 @@ def getArrayfromPath(image_path):
         array: array of the image
     """
     return sitk.GetArrayFromImage(sitk.ReadImage(image_path))
+
+def save_pickle(obj, path):
+    """Save object as pickle
+
+    Args:
+        obj (object): object to save
+        path (str): path to save object
+    """
+    with open(path, 'wb') as handle:
+        pickle.dump(obj, handle, pickle.HIGHEST_PROTOCOL)
+
+def open_pickle(path):
+    """Open pickle
+
+    Args:
+        path (str): path to pickle
+
+    Returns:
+        object: object in pickle
+    """
+    with open(path, 'rb') as handle:
+        return pickle.load(handle)
+    
+# Calculate TRE
+def calculate_tre(fixed_points, moving_points):
+    """Compute TRE
+    
+    args:
+        fixed_points (np.array): fixed points
+        moving_points (np.array): moving points
+    returns:
+        tre (np.array): TRE
+    
+    """
+    tre = np.sqrt(np.sum((fixed_points - moving_points) ** 2, axis=1))
+    return tre
+
